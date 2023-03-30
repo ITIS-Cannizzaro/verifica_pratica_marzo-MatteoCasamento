1f1ea065bd88167a00688170b0de37370b66103e
matte <matte@DESKTOP-68M5M0F>
2023-03-30 10:56:27 +0200

    rg

---
+++ src/Casamento.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Casamento
+{
+	static Scanner in = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		while(true)
+		{
+			stampaMenu();
+			int scelta = Integer.parseInt(in.nextLine());
+			switch(scelta)
+			{
+				
+				case 1:
+					MinimoDiArray();
+				break;
+				case 2:
+					ParoleMisteriose();
+				break;
+				case 3:
+					StampaPari();
+				break;
+				default:
+					System.out.println("Scelta errata, riprova!");
+
+			}
+		}
+	}
+	static void MinimoDiArray() 
+	{
+		int [] array = new int [10];
+		for(int i = 0; i < array.length; i++)
+		{
+			array [i] = (int) ((Math.random()*11)-5);
+		}
+		int min = array[0];
+		for(int i = 0; i<array.length; i++)
+		{
+			
+		}
+
+	}
+	static void stampaMenu()
+	{
+		System.out.println("1 - Minimo di Array");
+		System.out.println("2 - Parole Misteriose");
+		System.out.println("3 - Stampa Pari");
+		
+	}
+	
+
+}
